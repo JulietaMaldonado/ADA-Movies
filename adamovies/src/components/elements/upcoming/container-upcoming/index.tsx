5dac89d234490c768b6f07bc6df39d5f4182fc45
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2023-03-08 17:00:19 -0300

    Modificacion del elemento ContUpcoming

--- adamovies/src/components/elements/upcoming/container-upcoming/index.tsx
+++ adamovies/src/components/elements/upcoming/container-upcoming/index.tsx
@@ -1,29 +1,23 @@
-import { apiMovies } from "../../../../utils/axios";
-import { useEffect, useState } from "react";
+import { FC } from "react";
+import { BASE_URL, WIDTH_IMG } from "../../../../constants/movies-api";
 
-const ContUpcoming = () => {
-  const [movies, setMovies] = useState<any[]>([]);
+type Props = {
+  items: any[];
+  title: string;
+};
 
-  useEffect(() => {
-    apiMovies
-      .get("/movie/upcoming")
-      .then((response) => setMovies(response.data.results));
-  }, []);
-  const baseUrl = "https://image.tmdb.org/t/p/";
-  const widthImg = "w500";
-
+const ContUpcoming: FC<Props> = ({ items, title }) => {
   return (
     <>
-      {" "}
       <div className="title-upcoming">
-        <h2 className="title-upcoming2">UPCOMING</h2>
+        <h2 className="title-upcoming2">{title}</h2>
       </div>
       <div className="container-upcoming">
-        {movies.map((movie) => (
+        {items.map((movie) => (
           <div className="tarjeta-upcoming">
             <img
-              src={baseUrl + widthImg + movie.poster_path}
-              className="card-image-upcoming"
+              src={BASE_URL + WIDTH_IMG + movie.poster_path}
+              className="tarjeta-image-upcoming"
             />
           </div>
         ))}
--- adamovies/src/pages/Upcoming/index.tsx
+++ adamovies/src/pages/Upcoming/index.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
 import { Footer, Header, Main } from "../../components";
 import { ContUpcoming } from "../../components";
 import "./style.css";
+import { apiMovies } from "../../utils/axios";
 
 const Upcoming = () => {
+ const [upcoming, setUpcoming] = useState([]);
+ useEffect(() => {
+  apiMovies
+  .get("/movie/upcoming")
+  .then((response) => setUpcoming(response.data.results));
+ }, ([]))
+
   return (
     <>
       <Header />
       <Main>
-        <ContUpcoming />
+        <ContUpcoming items={upcoming} />
       </Main>
       <Footer />
     </>
